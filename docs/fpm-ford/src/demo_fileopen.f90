1cef007059cb1595ccc911d686c2064624382d2c
John S. Urban <urbanjost@comcast.net>
2023-04-16 17:58:43 -0400

    fpm 0.8.0

--- docs/fpm-ford/src/demo_fileopen.f90
+++
@@ -1,7 +0,0 @@
-        program demo_fileopen
-        use M_io, only : fileopen, fileclose, print_inquire
-        implicit none
-        integer :: lun
-        lun=fileopen('fred.txt')
-        call print_inquire(lun)
-        end program demo_fileopen
--- docs/fpm-ford/src/demo_get_env.f90
+++
@@ -1,7 +0,0 @@
-        program demo_get_env
-        use M_io, only : get_env
-        character(len=:),allocatable :: HOME
-           HOME=get_env('HOME','UNKNOWN')
-           write(*,'(a)')HOME,get_env('PATH')
-           write(*,'(a)')get_env('HOME'),get_env('PATH')
-        end program demo_get_env
