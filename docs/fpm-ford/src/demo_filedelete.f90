a2af5edfb0206a2ffcb9bd8b822973d6ab971870
urbanjost <urbanjost@comcast.net>
2026-08-21 03:32:41 +0000

    Deploying to gh-pages from  @ 6ceeeb81101f62e0dccc3b702660b20bc0567d9c 🚀

--- docs/fpm-ford/src/demo_filedelete.f90
+++
@@ -1,8 +0,0 @@
-      program demo_filedelete
-      use M_io, only : filedelete, fileopen
-      implicit none
-      integer :: lun
-      integer :: ios
-         lun=fileopen('<input.txt')
-         ios=filedelete(lun)
-      end program demo_filedelete
